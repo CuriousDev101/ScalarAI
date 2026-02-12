82f36f9fe35e13bdd06b2bcc0ce76ce1133d39b2
Adnan Ahmed <adnanahmedabdu@gmail.com>
2026-02-12 10:37:39 +0300

    feat(layout): basic layout fo nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,17 @@
 </script>
 
 <template>
-  <div>
-    NavBar
+  <div class="navbar bg-base-100 border-b border-gray-800">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">ScalarAI</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><a btn="btn">Login</a></li>
+        <li><a btn="btn">Signup</a></li>
+        <li><a btn="btn">Admin</a></li>
+        <li><a btn="btn">Logout</a></li>
+      </ul>
+    </div>
   </div>
 </template>
