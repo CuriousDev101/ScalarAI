2c54b876eab2bf37b499c913478851f3db156540
Adnan Ahmed <adnanahmedabdu@gmail.com>
2026-02-12 09:56:47 +0300

    chore: initial project setup

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import AdminView from "@/views/AdminView.vue";
+import HomeView from "@/views/HomeView.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: HomeView,
+		},
+		{
+			path: "/login",
+			name: "admin",
+			component: AdminView,
+		},
+	],
+});
+
+export default router;
